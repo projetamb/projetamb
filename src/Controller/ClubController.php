594df60777a570dfdbd0593f898553ac19598980
jean <perrault.jean66@gmail.com>
2019-04-15 13:51:30 +0200

    modif Club

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Personnal;
+use App\Repository\PersonnalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClubController extends AbstractController
 {
     /**
      * @Route("/club", name="club")
+     * @param PersonnalRepository $personnalRepository
+     * @return Response
      */
-    public function index()
+    public function show(PersonnalRepository $personnalRepository)
     {
+        $personnal = $personnalRepository->findAll();
         return $this->render('club/club.html.twig', [
             'pagetitle' => 'Arts Martiaux Basséens',
+            'personnals' => $personnal
         ]);
     }
 }
