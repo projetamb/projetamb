ca688312eb1613ad7dd78d0967b8c3f93468be96
vincent59480 <v.robin59@gmail.com>
2019-04-15 09:37:23 +0200

    ajout du login, du form enregistrement et du menu connexion deco

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,77 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/inscription", name="security_registration")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @param UserPasswordEncoderInterface $encoder
+     * @return \Symfony\Component\HttpFoundation\Response
+     * USerPasswordInterface permet d'encoder les MDP
      */
-    public function index()
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
-    }
+   public function registration(Request $request,ObjectManager $manager,UserPasswordEncoderInterface $encoder)
+   {
+       $user = new User(); // utilisateur vide
+       //on relie les champs de l'utilisateurs au formulaire
+        $form= $this->createForm(RegistrationType::class,$user);
+        $form->handleRequest($request);// je veux que le formulaire analyse la requete que je lui passe
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            //on demande d'encoder les MDP situés dans la class user. dans le security.yml on a le lien avec encoder
+            // dès que l'on a encoder, on va dans le fichier yml et on regarde sur quelle class on fait la relation. ici c'est la clss user
+            $hash=$encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($hash);// user je modifie ton MDP avec le hash
+            $manager->persist($user);
+            $manager->flush();
+            //
+            return $this->redirectToRoute('security_login');
+        }
+            return $this->render('security/registration.html.twig',[
+                'form'=> $form->createView()
+            ]);
+   }
+
+
+
+    /**
+     * @Route("/login",name="security_login")
+     *
+     */
+   public function loginAction(AuthenticationUtils $authenticationUtils)
+   {
+       // get the login error if there is one
+       $error = $authenticationUtils->getLastAuthenticationError();
+
+       // last username entered by the user
+       $lastUsername = $authenticationUtils->getLastUsername();
+       $this->addFlash(
+           'information',
+           'Connexion reussie'
+       );
+       return $this->render('security/login.html.twig', [
+           'last_username' => $lastUsername,
+           'error'         => $error,
+       ]);
+
+   }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+      public function logout(){
+
+   }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,20 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email","username"},
+ *     message="Champs identiques"
+ *     )
+ * on doit implement la class user pour pouvoir utiliser la class encoder
+ * on doit rajouter des methodes getRoles, erasecredential, getSalt
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -18,17 +27,42 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="4")<,message="votre username doit etre de 8 caracteres mini")
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     *@Assert\Length(min="8",minMessage="Votre mot de passe doit faire 8 caractères minimum")
+     *
      */
     private $password;
+    /**
+     * @Assert\EqualTo(propertyPath="password",message="mot de passe different")
+     */
+    private $confirm_password;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
     }
 
     public function getUsername(): ?string
@@ -53,5 +87,34 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConfirmPassword()
+    {
+        return $this->confirm_password;
+    }
+
+    /**
+     * @param mixed $confirm_password
+     */
+    public function setConfirmPassword($confirm_password): void
+    {
+        $this->confirm_password = $confirm_password;
+    }
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+    public function getRoles()
+    {
+        return ['ROLE_ADMIN'];
+        // TODO: Implement getRoles() method.
     }
 }
