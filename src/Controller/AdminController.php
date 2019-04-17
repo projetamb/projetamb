c80c2cfede55520923dace855b5913758a8d9fa7
vincent59480 <v.robin59@gmail.com>
2019-04-17 14:05:33 +0200

    add mode admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Events;
 use Doctrine\Common\Persistence\ObjectManager;
-use http\Env\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,6 +29,36 @@ class AdminController extends AbstractController
 
         return $this->render('admin/admin.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/event", name="security_FormEvent")
+     * @param Request $request
+     * @param ObjectManager $manager
+     */
+    public function form(Request $request, ObjectManager $manager)
+    {
+        $evenement = new Events();
+        // je crée un form qui est est lié a mon évenement
+        $form= $this->createFormBuilder($evenement)
+                    ->add('title')
+                    ->add('place')
+                    ->add('organisator')
+                    ->add('description')
+                    ->add('date',DateType::class)
+                    ->add('email_contact')
+                    ->add('phone_contact')
+                    ->add('photo',FileType::class)
+                    ->getForm();
+        $form->handleRequest($request);
+            dump($request);
+            /*if($form->isSubmitted() && $form->isValid()){
+                $manager->persist($form);
+                $manager->flush();
+            }*/
+        return $this->render('admin/formEvent.html.twig',[
+            'formEvent'=>$form->createView()
         ]);
     }
 }
