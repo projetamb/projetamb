e8bec9824ae34de6e0b4ef5a2e0cb0dafd7818db
vincent59480 <v.robin59@gmail.com>
2019-04-18 14:53:07 +0200

    ajout de la suppression des membres du bureau + instructeur

--- src/Entity/Events.php
+++ src/Entity/Events.php
@@ -126,12 +126,12 @@ class Events
         return $this;
     }
 
-    public function getPhoto(): ?string
+    public function getPhoto()
     {
         return $this->photo;
     }
 
-    public function setPhoto(string $photo): self
+    public function setPhoto( $photo): self
     {
         $this->photo = $photo;
 
